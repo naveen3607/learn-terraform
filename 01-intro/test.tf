862c98a01f5d2edb1528d76b869881b48d5d8dc2
naveen3607 <naveenreddykallam3607@gmail.com>
2023-08-02 09:07:03 -0400

    Terraform Script

--- 01-intro/test.tf
+++ 01-intro/test.tf
@@ -2,3 +2,7 @@
 # terraform plan - Will tell the execution plan related to the planning to do when user actually applied.
 # terraform apply - Create the resources which are mentioned in the code. If the resource is already available, then TF will not do anything.
 # terraform destroy (Not used in companies mostly) - Delete the resources which were been created so far by TF. However TF will not look for code to delete, it will delete created resources by TF.
+
+output "test" {
+  value = "Hello World"
+}
