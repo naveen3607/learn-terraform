569ea0e7f84d8fdf59f74655f17e6eb83cc0f06b
naveen3607 <naveenreddykallam3607@gmail.com>
2023-08-02 09:06:08 -0400

    Terraform Script

--- 01-intro/test.tf
+++ 01-intro/test.tf
@@ -1,4 +1,4 @@
-# terraform init
-# terraform plan
-# terraform apply
-# terraform destroy (Not used in companies mostly)
+# terraform init - Will understand the code written by user and download necessary provider plugins.
+# terraform plan - Will tell the execution plan related to the planning to do when user actually applied.
+# terraform apply - Create the resources which are mentioned in the code. If the resource is already available, then TF will not do anything.
+# terraform destroy (Not used in companies mostly) - Delete the resources which were been created so far by TF. However TF will not look for code to delete, it will delete created resources by TF.
