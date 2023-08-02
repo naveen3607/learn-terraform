b3514d506a07f349c5474eb0782913928e36d462
naveen3607 <naveenreddykallam3607@gmail.com>
2023-08-02 15:30:06 -0400

    Terraform Script

---
+++ Roboshop/roboshop.tf
@@ -0,0 +1,98 @@
+resource "aws_instance" "frontend" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "01-Frontend"
+  }
+}
+
+resource "aws_instance" "mongodb" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "02-MongoDB"
+  }
+}
+
+resource "aws_instance" "catalogue" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "03-Catalogue"
+  }
+}
+
+resource "aws_instance" "redis" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "04-Redis"
+  }
+}
+
+resource "aws_instance" "user" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "05-User"
+  }
+}
+
+resource "aws_instance" "cart" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "06-Cart"
+  }
+}
+
+resource "aws_instance" "mysql" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "07-MySQL"
+  }
+}
+
+resource "aws_instance" "shipping" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "08-Shipping"
+  }
+}
+
+resource "aws_instance" "rabbitmq" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "09-RabbitMQ"
+  }
+}
+
+resource "aws_instance" "payment" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "10-Payment"
+  }
+}
+
+resource "aws_instance" "dispatch" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "11-Dispatch"
+  }
+}
