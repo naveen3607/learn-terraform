861d1fbb0d0d5cf04d57586e63989ab71ed1508e
naveen3607 <naveenreddykallam3607@gmail.com>
2023-08-04 06:05:49 -0400

    Terraform Script

---
+++ 06-loops/for-each/main.tf
@@ -0,0 +1,24 @@
+variable "components" {
+  default = {
+    frontend = {name = "frontend"}
+    mongodb = {name = "mongodb"}
+  }
+}
+
+resource "aws_instance" "instance" {
+
+  for_each = var.components
+
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [ "sg-094473d55ab4bd85d" ]
+
+  tags = {
+    name = lookup(var.components, each.value["name"], null)
+  }
+}
+
+resource "aws_security_group" "allow_tls" {
+  for_each = var.components
+  name = lookup(var.components, each.value["name"], null)
+}
