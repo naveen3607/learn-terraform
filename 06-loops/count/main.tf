3b51fe78d25c0ff577d642e5e9ac7e61368f9611
naveen3607 <naveenreddykallam3607@gmail.com>
2023-08-04 05:41:41 -0400

    Terraform Script

---
+++ 06-loops/count/main.tf
@@ -0,0 +1,16 @@
+variable "components" {
+  default = [ "frontend", "mongodb", "catalogue" ]
+}
+
+resource "aws_instance" "instance" {
+
+  count = length(var.components)
+
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [ "sg-094473d55ab4bd85d" ]
+
+  tags = {
+    Name = " "
+  }
+}
