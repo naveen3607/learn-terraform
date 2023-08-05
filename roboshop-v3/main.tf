37376df0a1e7932553793c0cf53473aa6f0421fe
naveen3607 <naveenreddykallam3607@gmail.com>
2023-08-05 05:52:50 -0400

    Terraform Script

--- roboshop-v3/main.tf
+++ roboshop-v3/main.tf
@@ -1,7 +1,8 @@
 module "components" {
+  for_each = var.components
   source = "./module"
-  instance_type = var.instance_type
-  name = var.name
+  instance_type = each.value["instance_type"]
+  name = each.value["name"]
   zone_id = var.zone_id
   security_group = var.security_group
 }
--- roboshop-v3/vars.tf
+++ roboshop-v3/vars.tf
@@ -10,7 +10,7 @@ variable "instance_type" {
   default = "t2.micro"
 }
 
-variable "name" {
+variable "components" {
   default = {
     frontend = {
       name = "frontend-dev"
