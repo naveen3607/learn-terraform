f229aa8375672b409784a05b37ad602c82f8de1c
naveen3607 <naveenreddykallam3607@gmail.com>
2023-08-05 05:13:39 -0400

    Terraform Script

--- 07-modules/local_module/data.tf
+++ 07-modules/local_module/data.tf
@@ -1,8 +1,14 @@
-data "aws_ami_ids" "ami" {
-  name_regex  = "Centos-8"
+data "aws_ami" "ami" {
+  name_regex  = "Centos-8-DevOps-Practice"
   owners      = ["973714476881"]
 }
 
 output "ami" {
-  value = data.aws_ami_ids.ami
+  value = data.aws_ami.ami.id
+}
+
+resource "aws_instance" "instance" {
+  ami = data.aws_ami.ami.id
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [ "sg-094473d55ab4bd85d" ]
 }
