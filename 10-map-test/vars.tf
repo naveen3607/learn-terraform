4368c5d8a727c177dfd3ded39a96d4867ef17dce
naveen3607 <naveenreddykallam3607@gmail.com>
2023-08-19 10:24:08 -0400

    Terraform Script

---
+++ 10-map-test/vars.tf
@@ -0,0 +1,26 @@
+variable "vpc" {
+  default = {
+    main = {
+      cidr = "10.0.0.0/16"
+      az = [ "us-east-1a", "us-east-1b" ]
+      subnets = {
+        public = {
+          public1 = { cidr = "10.0.0.0/16", az = "us-east-1a" }
+          public2 = { cidr = "10.0.1.0/16", az = "us-east-1b" }
+        }
+        app = {
+          app1 = { cidr = "10.0.2.0/16", az = "us-east-1a" }
+          app2 = { cidr = "10.0.3.0/16", az = "us-east-1b" }
+        }
+        db = {
+          db1 = { cidr = "10.0.4.0/16", az = "us-east-1a" }
+          db2 = { cidr = "10.0.5.0/16", az = "us-east-1b" }
+        }
+      }
+    }
+  }
+}
+
+output "vpc" {
+  value = var.vpc["main"]["subnets"]
+}
