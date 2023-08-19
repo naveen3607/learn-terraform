c50d7421a1e844d93885b38b4376ab775e0e43a1
naveen3607 <naveenreddykallam3607@gmail.com>
2023-08-19 10:44:03 -0400

    Terraform Script

--- 10-map-test/vars.tf
+++ 10-map-test/vars.tf
@@ -21,6 +21,12 @@ variable "vpc" {
   }
 }
 
+locals {
+  public = { for k,v in merge(var.vpc["main"]["subnets"]["public"]) : k => v }
+  app = { for k,v in merge(var.vpc["main"]["subnets"]["app"]) : k => v }
+  db = { for k,v in merge(var.vpc["main"]["subnets"]["db"]) : k => v }
+}
+
 output "all_subnets" {
-  value = { for k,v in merge(var.vpc["main"]["subnets"]) : k => v }
+  value = merge(local.public, local.app, local.db)
 }
